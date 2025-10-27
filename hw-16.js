f54a3c17d0826c0ae9aa40772d9beb3975115878
Nazar Bekerskij <bekerskijnazar9@gmail.com>
2025-10-27 22:13:45 +0200

    1123

--- hw-16.js
+++ hw-16.js
@@ -12,7 +12,7 @@ Account.prototype.getInfo = function () {
 
 
 
-console.log(Account.prototype.getInfo); // function
+console.log(typeof Account.prototype.getInfo); // function
 
 const mango = new Account({
  login: 'Mangozedog',
@@ -27,3 +27,49 @@ const poly = new Account({
 });
 
 poly.getInfo(); // Login: Poly, Email: poly@mail.com
+
+
+
+
+
+
+
+// Напиши клас User для створення користувача з наступними властивостями:
+
+
+
+// name — рядок
+// age — число
+// followers — число
+
+
+// Добав метод getInfo(), який, виводить рядок: User ${ім'я} is ${вік} years old and has ${кількість фоловерів} followers
+
+class User {
+    constructor({name, age, followers}){
+        this.name = name;
+        this.age = age;
+        this.followers = followers;
+    }
+    getInfo(){
+    console.log(`User: ${this.name} is ${this.age} yers old and has ${this.followers}`);
+}
+}
+
+
+
+const mangoAccount = new User({
+ name: 'Mango',
+ age: 2,
+ followers: 20,
+});
+
+mangoAccount.getInfo(); // User Mango is 2 years old and has 20 followers
+
+const polyAccount = new User({
+ name: 'Poly',
+ age: 3,    
+ followers: 17,
+});
+
+polyAccount.getInfo(); // User Poly is 3 years old and has 17 followers
