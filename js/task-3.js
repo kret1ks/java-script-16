056af8685b1dee2efa3264c9b03b610d74a592c6
Nazar Bekerskij <bekerskijnazar9@gmail.com>
2025-10-30 21:47:33 +0200

    123

---
+++ js/task-3.js
@@ -0,0 +1,40 @@
+// Додай методи класу:
+
+
+
+// getItems() — повертає масив поточних товарів
+// addItem(item) — отримує новий товар і додає його до поточних
+// removeItem(item) — отримує товар і, якщо він є, видаляє його з поточних
+
+class Storage {
+    constructor(intialItems){
+        this.items = intialItems
+    }
+    getItems(){
+        return this.items;
+    }
+    addItem(item){
+        this.items.push(item)
+    }
+    removeItem(item){
+        this.item = this.items.filter(product => product !== item)
+    }
+}
+
+
+
+const storage = new Storage([
+ 'Нанітоіди',
+ 'Пролонгер',
+ 'Залізні жупи',
+ 'Антигравітатор',
+]);
+
+const items = storage.getItems();
+console.table(items); // [ "Нанітоіди", "Пролонгер", "Залізні жупи", "Антигравітатор" ]
+
+storage.addItem('Дроїд');
+console.table(storage.items); // [ "Нанітоіди", "Пролонгер", "Залізні жупи", "Антигравітатор", "Дроїд" ]
+
+storage.removeItem('Пролонгер');
+console.table(storage.items); // [ "Нанітоіди", "Залізні жупи", "Антигравітатор", "Дроїд" ]
